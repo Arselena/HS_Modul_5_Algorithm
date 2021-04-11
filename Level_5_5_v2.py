94cca3570700682b8711d1ba15cf853a1cc1ec21
Arselena <efedorovyh@yandex.ru>
2021-04-11 20:03:10 +0500

    5. Очереди_v2. Del import Stack

--- Level_5_5_v2.py
+++ Level_5_5_v2.py
@@ -4,7 +4,7 @@
 # enqueue(item) -- добавить элемент в хвост очереди, 
 # и dequeue(), которая возвращает элемент из головы очереди, удаляя его.
 import ctypes
-from Level_5_4 import Stack
+# from Level_5_4 import Stack
 
 class Queue:
     def __init__(self): # инициализация хранилища данных
@@ -46,6 +46,44 @@ def round(qu:Queue(), n:int):
         n -= 1
 
 # 6.4. Попробуйте реализовать очередь с помощью двух стеков.
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def make_stack(self, new_capacity):
+        return (new_capacity * ctypes.py_object)()
+    
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        size = self.size()
+        if size == 0:
+            return None # если стек пустой
+        new_stack = ((self.size() - 1) * ctypes.py_object)()
+        for i in range(size - 1):
+            new_stack[i] = self.stack[i]
+        end_element = self.stack[size - 1]
+        self.stack = new_stack
+        return end_element
+
+    def push(self, value):
+        new_stack = ((self.size() + 1) * ctypes.py_object)()  # расширяем массив памяти на 1 ячейку
+        size = self.size()
+        for i in range(size):
+            new_stack[i] = self.stack[i]
+        new_stack[size] = value
+        self.stack = new_stack
+
+    def peek(self):
+        size = self.size()
+        if size == 0:
+            return None # если стек пустой
+        return self.stack[size - 1]
+
+    def print_all(self):
+        for i in range(self.size()):
+            print(self.stack[i])
 
 class queue_by_stack():
     def __init__(self):
